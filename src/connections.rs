be59b3c5fbee1ba6ba09c5494dff9a9946f3d3a0
Droppel <leagueoflegends@sofortstart.de>
2026-02-02 09:55:29 +0100

    Add two more items back in

--- src/connections.rs
+++ src/connections.rs
@@ -342,21 +342,21 @@ pub fn setup_item_placements(base_regions: &mut [BaseRegion], item_locations: &[
         vec!["Shovel".to_string()],
     ));
 
-    // // Item 3: Bomb
-    // base_regions[item_locations[3]].add_statechange(StateChange::new(
-    //     vec!["has_bomb".to_string()],
-    //     vec![true],
-    //     rules::can_pickup_bomb,
-    //     vec!["Bomb".to_string()],
-    // ));
+    // Item 3: Bomb
+    base_regions[item_locations[3]].add_statechange(StateChange::new(
+        vec!["has_bomb".to_string()],
+        vec![true],
+        rules::can_pickup_bomb,
+        vec!["Bomb".to_string()],
+    ));
 
-    // // Item 4: Shield
-    // base_regions[item_locations[4]].add_statechange(StateChange::new(
-    //     vec!["has_shield".to_string()],
-    //     vec![true],
-    //     rules::can_pickup_shield,
-    //     vec!["Shield".to_string()],
-    // ));
+    // Item 4: Shield
+    base_regions[item_locations[4]].add_statechange(StateChange::new(
+        vec!["has_shield".to_string()],
+        vec![true],
+        rules::can_pickup_shield,
+        vec!["Shield".to_string()],
+    ));
 
     // // Item 5: Mister Hugs
     // base_regions[item_locations[5]].add_statechange(StateChange::new(
